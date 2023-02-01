e4f231d788625faf1c610b44d119877e77d00e5f
surkov <surkovdennis@gmail.com>
2023-02-01 23:07:53 +0300

    workshop refactoring

--- Image Feed/ImagesList/ImagesListCell.swift
+++ Image Feed/ImagesList/ImagesListCell.swift
@@ -1,8 +1,17 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
-    static let reuseIdentifier = "ImagesListCell"
-    @IBOutlet weak var cellImage: UIImageView!
-    @IBOutlet weak var likeButton: UIButton!
-    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet private weak var cellImage: UIImageView!
+    @IBOutlet private weak var likeButton: UIButton!
+    @IBOutlet private weak var dateLabel: UILabel!
+}
+
+extension ImagesListCell {
+    func configure(image: UIImage?, date: String, isLiked: Bool) {
+        cellImage.image = image
+        dateLabel.text = date
+
+        let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
+        likeButton.setImage(likeImage, for: .normal)
+    }
 }
--- Image Feed/ImagesList/ImagesListViewController.swift
+++ Image Feed/ImagesList/ImagesListViewController.swift
@@ -1,12 +1,23 @@
 import UIKit
 
 class ImagesListViewController: UIViewController {
-    @IBOutlet private var tableView: UITableView!
+    private struct Constants {
+        static let id = String(describing: ImagesListCell.self)
+    }
+    
+    @IBOutlet private var tableView: UITableView! {
+        didSet {
+            tableView.register(UINib(nibName: Constants.id, bundle: nil), forCellReuseIdentifier: Constants.id)
+        }
+    }
 
     private var photosName = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
 
         photosName = Array(0..<20).map{ "\($0)" }
     }
@@ -25,25 +36,21 @@ extension ImagesListViewController: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath) as? ImagesListCell
-
-        configCell(for: imageListCell, with: indexPath)
-
+        guard let imageListCell = tableView.dequeueReusableCell(withIdentifier: Constants.id, for: indexPath) as? ImagesListCell else {
+            return UITableViewCell()
+        }
+        
+        let image = UIImage(named: photosName[indexPath.row])
+        let date = dateFormatter.string(from: Date())
+        let isLiked = indexPath.row % 2 == 0
+        imageListCell.configure(image: image, date: date, isLiked: isLiked)
+        
         return imageListCell
     }
 }
 
-extension ImagesListViewController {
-    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
-        guard let image = UIImage(named: photosName[indexPath.row]) else {
-            return
-        }
-
-        cell.cellImage.image = image
-        cell.dateLabel.text = dateFormatter.string(from: Date())
-
-        let isLiked = indexPath.row % 2 == 0
-        let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
-        cell.likeButton.setImage(likeImage, for: .normal)
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
